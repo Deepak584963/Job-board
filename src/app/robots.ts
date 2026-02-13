65a011f8e428e3a97fc1faf2445f0bf08f717bc4
Deepak584953 <dk059511@gmail.com>
2026-02-13 09:58:55 +0530

    roboto change

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://jobboard.example.com/sitemap.xml",
+    sitemap: "https://job-board-one-chi.vercel.app/sitemap.xml",
   };
 }
